3cfdbd962095a54fd0109ba6a78050ae6e1aaf33
gouthamsgit <goutham.s@f22labs.com>
2022-12-15 18:13:18 +0530

    env changes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
-require("dotenv").config();
-const port = process.env.PORT;
+// require("dotenv").config();
+const port = 3000;
 app.get("/", (req, res) => {
   res.send("sample express js app");
 });
