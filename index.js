d90c08549815ce0318061056d83c4aad38e078c3
gouthamsgit <goutham.s@f22labs.com>
2022-12-15 13:42:21 +0530

    port changed

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
-const port = process.env.PORT;
-
+// const port = process.env.PORT;
+const port = 3000;
 app.get("/", (req, res) => {
   res.send("Sample express js app");
 });
