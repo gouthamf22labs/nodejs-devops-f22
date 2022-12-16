3e2567e11b7b689a79328ae416efe41d7e63b728
gouthamsgit <goutham.s@f22labs.com>
2022-12-16 17:11:24 +0530

    log changes

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.status(200).send("Sample express app World!");
+  res.status(200).send("Dummy express app World!");
 });
 
 module.exports = app;
