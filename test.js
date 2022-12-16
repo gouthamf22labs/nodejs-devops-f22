67207de875aa11d55d179aad110439aacb4fb31a
gouthamsgit <goutham.s@f22labs.com>
2022-12-16 16:05:45 +0530

    testing jest

--- test.js
+++ test.js
@@ -3,6 +3,6 @@ const app = require("./index");
 
 describe("Test the root path", () => {
   test("It should response the GET method", () => {
-    return request(app).get("/").expect(300);
+    return request(app).get("/").expect(200);
   });
 });
