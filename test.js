04381a4a15e4d1ee9502709a18795b5ff1fee133
gouthamsgit <goutham.s@f22labs.com>
2022-12-19 15:49:37 +0530

    test file changes

--- test.js
+++ test.js
@@ -2,7 +2,12 @@ const request = require("supertest");
 const app = require("./index");
 
 describe("Test the root path", () => {
-  test("It should response the GET method", () => {
-    return request(app).get("/").expect(200);
+  test("It should response the GET method", (done) => {
+    request(app)
+      .get("/")
+      .then((response) => {
+        expect(response.statusCode).toBe(200);
+        done();
+      });
   });
 });
